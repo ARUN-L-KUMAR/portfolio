0add77db2c695819daa024af2fda22af0844947a
ARUN-L-KUMAR <arunkumar582004@gmail.com>
2025-07-01 16:14:43 +0530

    vite config fixed for vercel deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/",  // Change this to match your repo name
+  base: "/",  // Change this to match your repo name
 });
